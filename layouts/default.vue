9dc6ea6af0e3be54e03252a8a3c913d9de9c3263
Aoshi Fujioka <blue20will@gmail.com>
2022-06-28 19:50:33 +0900

    feat: update base layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,49 @@
 <template>
     <a-layout>
-        <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
-            <a-menu
-                theme="dark"
-                mode="horizontal"
-                :style="{ lineHeight: '64px' }"
-            >
+        <a-layout-sider theme="dark" :style="{ height: '100vh' }" v-model="collapsed" :trigger="null" collapsible>
+            <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
                 <a-menu-item key="1">
-                    <NuxtLink to="/members">雀士一覧</NuxtLink>
+                    <NuxtLink to="/members">
+                        <a-icon type="user" />
+                        <span>雀士一覧</span>
+                    </NuxtLink>
                 </a-menu-item>
                 <a-menu-item key="2">
-                    <NuxtLink to="/mahjong/matches">戦一覧</NuxtLink>
+                    <NuxtLink to="/mahjong/matches">
+                        <a-icon type="video-camera" />
+                        <span>戦一覧</span>
+                    </NuxtLink>
                 </a-menu-item>
                 <a-menu-item key="3">
-                    <NuxtLink to="/mahjong/games">局一覧</NuxtLink>
-                </a-menu-item>
-                <a-menu-item key="4">
-                    <NuxtLink to="/mahjong/jansous">雀荘一覧</NuxtLink>
+                    <NuxtLink to="/mahjong/games">
+                        <a-icon type="upload" />
+                        <span>戦績一覧</span>
+                    </NuxtLink>
                 </a-menu-item>
             </a-menu>
-        </a-layout-header>
-        <a-layout-content :style="{ padding: '0 50px', marginTop: '64px' }">
-            <a-breadcrumb :style="{ margin: '16px 0' }">
-                <a-breadcrumb-item>Home</a-breadcrumb-item>
-                <a-breadcrumb-item>List</a-breadcrumb-item>
-                <a-breadcrumb-item>App</a-breadcrumb-item>
-            </a-breadcrumb>
-            <div :style="{ background: '#fefefe', padding: '24px', minHeight: '380px' }">
-                <Nuxt />
-            </div>
-        </a-layout-content>
-        <a-layout-footer :style="{ textAlign: 'center' }">
-            fidop-yakuman ©2022
-        </a-layout-footer>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0">
+                <a-icon
+                    :style="{ padding: '0 3em' }"
+                    :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                    @click="() => (collapsed = !collapsed)"
+                />
+            </a-layout-header>
+            <a-layout-content
+                :style="{ margin: '3em 2em', padding: '3em', background: '#fff' }"
+            >
+            <Nuxt />
+            </a-layout-content>
+        </a-layout>
     </a-layout>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
